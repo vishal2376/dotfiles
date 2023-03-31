91648e7e88865b9ace2bd900b25e51c960528137
Vishal Singh <vishalsingh2376@gmail.com>
2023-03-31 09:43:27 +0530

    add: script to copy config files

---
+++ copy_files.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -r ~/.config/btop ~/.config/dunst ~/.config/hypr/ ~/.config/neofetch/ ~/.config/rofi ~/.config/swaylock ~/.config/waybar ~/.config/cava/ /mnt/ECHO/dotfiles/.config/
+
+cp -r ~/.config/nvim/lua/core/ /mnt/ECHO/dotfiles/nvim/lua/
+cp -r ~/.config/nvim/lua/custom/ /mnt/ECHO/dotfiles/nvim/lua/
