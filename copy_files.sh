003c40e6683469ccf2c118a3edec32b27c78683c
Vishal Singh <vishalsingh2376@gmail.com>
2023-03-31 18:45:37 +0530

    update: check and create required nvim folder

--- copy_files.sh
+++ copy_files.sh
@@ -2,5 +2,5 @@
 
 cp -r ~/.config/btop ~/.config/dunst ~/.config/hypr/ ~/.config/neofetch/ ~/.config/rofi ~/.config/swaylock ~/.config/waybar ~/.config/cava/ /mnt/ECHO/dotfiles/.config/
 
-cp -r ~/.config/nvim/lua/core/ /mnt/ECHO/dotfiles/nvim/lua/
-cp -r ~/.config/nvim/lua/custom/ /mnt/ECHO/dotfiles/nvim/lua/
+[ -d "/mnt/ECHO/dotfiles/.config/nvim/lua/" ] || mkdir -pv /mnt/ECHO/dotfiles/.config/nvim/lua/
+cp -r ~/.config/nvim/lua/custom/ /mnt/ECHO/dotfiles/.config/nvim/lua/
