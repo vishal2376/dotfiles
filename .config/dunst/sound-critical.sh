f479c4b9c410a9bde976e17afd4e25f04b6b4a81
Vishal Singh <vishalsingh2376@gmail.com>
2023-04-07 15:55:49 +0530

    update: add notification sounds

---
+++ .config/dunst/sound-critical.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+paplay ~/.local/share/sounds/notification/pikachu_notification_tone.ogg
---
+++ .config/dunst/sound-normal.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+paplay ~/.local/share/sounds/notification/simple_notification.ogg
