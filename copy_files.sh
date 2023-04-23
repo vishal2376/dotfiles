e8cd1d65587b51617394fd51a36004f594d46c58
Vishal Singh <vishalsingh2376@gmail.com>
2023-04-23 13:19:47 +0530

    update: copy scripts folder

--- copy_files.sh
+++ copy_files.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+#config files
 cp -r ~/.config/btop ~/.config/dunst ~/.config/hypr/ ~/.config/neofetch/ ~/.config/rofi ~/.config/swaylock ~/.config/waybar ~/.config/cava/ /mnt/ECHO/dotfiles/.config/
 
+#neovim config
 [ -d "/mnt/ECHO/dotfiles/.config/nvim/lua/" ] || mkdir -pv /mnt/ECHO/dotfiles/.config/nvim/lua/
 cp -r ~/.config/nvim/lua/custom/ /mnt/ECHO/dotfiles/.config/nvim/lua/
+
+#custom scripts
+cp -r ~/Documents/Bash/scripts /mnt/ECHO/dotfiles
