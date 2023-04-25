61cf5e2edec33d55d8079da41d3f1567a78ce9a6
Vishal Singh <vishalsingh2376@gmail.com>
2023-04-25 14:15:10 +0530

    fix: rust autocompletions

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -13,5 +13,12 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"rust"},
+  root_dir = lspconfig.util.root_pattern("Cargo.toml")
+}) 
+
 -- 
 -- lspconfig.pyright.setup { blabla}
